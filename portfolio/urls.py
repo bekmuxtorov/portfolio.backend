a2b1ed95fed77ab2d9ec55aac94210ca99e93bbf
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-06-28 14:23:11 +0500

    ✏️ Added urlpatterns

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('projects/', views.ProjectListAPIView.as_view()),
     path('projects/<int:pk>/', views.ProjectDetailAPIView.as_view()),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
